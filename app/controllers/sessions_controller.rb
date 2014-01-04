fbe023fcba83ee2a8b895a65f58fc2cd5f37bdf2
Chris Fenton <christopherfenton4@gmail.com>
2014-01-04 13:38:06 -0500

    working sign in, plus added non working nav links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
 	def create
 		client = Client.find_by(email:params[:session][:email].downcase)
 		if client && client.authenticate(params[:session][:password])
-			sign_in user
-			redirect_to user
+			sign_in client
+			redirect_to client
 		else
 			#flash.now makes sure the flash isn't persistent
 			flash.now[:error] = 'Invalid email/password combination'
